6b0cd4036a289448d7ea4bd5d086678d83d5565c
xphade <18196286+xphade@users.noreply.github.com>
2021-09-24 15:42:44 +0200

    Add initial files

---
+++ bin/dart_euler.dart
@@ -0,0 +1,5 @@
+import 'package:dart_euler/dart_euler.dart' as dart_euler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_euler.calculate()}!');
+}
---
+++ test/dart_euler_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_euler/dart_euler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
