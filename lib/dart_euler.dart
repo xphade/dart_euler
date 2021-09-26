510271f5b15dbe21c49d9be66e23a63235bc927d
xphade <18196286+xphade@users.noreply.github.com>
2021-09-26 14:33:29 +0200

    Add solution and tests for problem 2
    Additionally refactored the unit tests slightly.

--- lib/dart_euler.dart
+++ lib/dart_euler.dart
@@ -114,10 +114,8 @@ LCSResult longestCollatzSequence(int limit) {
 ///
 /// The function calculates the Fibonacci sequence until the current number
 /// contains the given [nDigits] digits. It then returns the index of this
-/// number.
-///
-/// The Fibonacci sequence is defined by `F_n = F_{n-2} + F_{n-1}` where `f_1 =
-/// f_2 = 1`.
+/// number. For a definition of the Fibonacci sequence, see the documentation of
+/// [evenFibonacciNumbers].
 ///
 /// Example:
 /// ```dart
@@ -191,4 +189,40 @@ int sumSquareDifference(int maxNum) {
       numberSequence.reduce((value, number) => value + number), 2) as int;
 
   return squaredSum - sumOfSquares;
+}
+
+/// Calculates the sum of all even Fibonacci numbers not exceeding [limit].
+///
+/// The function calculates the Fibonacci sequence up until the given [limit]
+/// and returns the sum of all the even numbers. It asserts that the [limit] is
+/// non-negative.
+///
+/// The Fibonacci sequence is defined by `F_n = F_{n-2} + F_{n-1}` where `f_1 =
+/// f_2 = 1`.
+///
+/// Example:
+/// ```dart
+/// // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+/// evenFibonacciNumbers(20) == 10  // 2 + 8
+/// evenFibonacciNumbers(100) == 44 // 2 + 8 + 34
+/// ```
+int evenFibonacciNumbers(int limit) {
+  assert(!limit.isNegative);
+
+  // 2 is the first even number in the Fibonacci sequence.
+  if (limit < 2) return 0;
+
+  var prevNumber = 1;
+  var curNumber = 2;
+  var sum = 0;
+
+  while (curNumber <= limit) {
+    if (curNumber.isEven) sum += curNumber;
+
+    final nextNumber = prevNumber + curNumber;
+    prevNumber = curNumber;
+    curNumber = nextNumber;
+  }
+
+  return sum;
 }
--- test/dart_euler_test.dart
+++ test/dart_euler_test.dart
@@ -1,11 +1,11 @@
 import 'package:dart_euler/dart_euler.dart';
 import 'package:test/test.dart';
 
-Matcher throwsAssertion() => throwsA(const TypeMatcher<AssertionError>());
+Matcher throwsAssertion = throwsA(isA<AssertionError>());
 
 void main() {
   test('factorialDigitSum', () {
-    expect(() => factorialDigitSum(-1), throwsAssertion());
+    expect(() => factorialDigitSum(-1), throwsAssertion);
     expect(factorialDigitSum(0), 1);
     expect(factorialDigitSum(5), 3);
     expect(factorialDigitSum(10), 27);
@@ -13,17 +13,17 @@ void main() {
   });
 
   test("smallestMultiple", () {
-    expect(() => smallestMultiple(2, -3), throwsAssertion());
-    expect(() => smallestMultiple(-3, -2), throwsAssertion());
-    expect(() => smallestMultiple(3, 2), throwsAssertion());
+    expect(() => smallestMultiple(2, -3), throwsAssertion);
+    expect(() => smallestMultiple(-3, -2), throwsAssertion);
+    expect(() => smallestMultiple(3, 2), throwsAssertion);
     expect(smallestMultiple(2, 2), 2);
     expect(smallestMultiple(1, 10), 2520);
     expect(smallestMultiple(1, 20), 232792560);
   });
 
   test("longestCollatzSequence", () {
-    expect(() => longestCollatzSequence(-1), throwsAssertion());
-    expect(() => longestCollatzSequence(0), throwsAssertion());
+    expect(() => longestCollatzSequence(-1), throwsAssertion);
+    expect(() => longestCollatzSequence(0), throwsAssertion);
 
     var res = longestCollatzSequence(10);
     expect(res.longestChainNumber, 9);
@@ -35,15 +35,15 @@ void main() {
   });
 
   test("nDigitsFibonacciNumber", () {
-    expect(() => nDigitFibonacciNumber(-1), throwsAssertion());
-    expect(() => nDigitFibonacciNumber(0), throwsAssertion());
+    expect(() => nDigitFibonacciNumber(-1), throwsAssertion);
+    expect(() => nDigitFibonacciNumber(0), throwsAssertion);
     expect(nDigitFibonacciNumber(1), 1);
     expect(nDigitFibonacciNumber(3), 12);
     expect(nDigitFibonacciNumber(1000), 4782);
   });
 
   test("sumOfMultiples", () {
-    expect(() => sumOfMultiples(-1), throwsAssertion());
+    expect(() => sumOfMultiples(-1), throwsAssertion);
     expect(sumOfMultiples(0), 0);
     expect(sumOfMultiples(2), 0);
     expect(sumOfMultiples(10), 23);
@@ -51,10 +51,19 @@ void main() {
   });
 
   test("sumSquareDifference", () {
-    expect(() => sumSquareDifference(-1), throwsAssertion());
-    expect(() => sumSquareDifference(0), throwsAssertion());
+    expect(() => sumSquareDifference(-1), throwsAssertion);
+    expect(() => sumSquareDifference(0), throwsAssertion);
     expect(sumSquareDifference(1), 0);
     expect(sumSquareDifference(10), 2640);
     expect(sumSquareDifference(100), 25164150);
+  });
+
+  test("evenFibonacciNumber", () {
+    expect(() => evenFibonacciNumbers(-1), throwsAssertion);
+    expect(evenFibonacciNumbers(0), 0);
+    expect(evenFibonacciNumbers(1), 0);
+    expect(evenFibonacciNumbers(2), 2);
+    expect(evenFibonacciNumbers(100), 44);
+    expect(evenFibonacciNumbers(4000000), 4613732);
   });
 }
